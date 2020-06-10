a20f915bb3cd2958728c7ec425b4573eb0eb6908
Jeffrey Yohan Tobon Cataño <jtobon@cidenet.com.co>
2020-06-10 12:05:46 -0500

    delete duplicate directory

--- api-rest-pnc-rias/src/main/java/co/com/apirest/rias/models/dao/IStepDAO.java
+++
@@ -1,9 +0,0 @@
-package co.com.apirest.rias.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import co.com.apirest.rias.models.entity.StepEntity;
-
-public interface IStepDAO extends CrudRepository<StepEntity, Long> {
-
-}
